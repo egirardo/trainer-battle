8b4285fdf6a64db8febdceb9d28fca34aeb6ec74
Elsa Girardo <egirardo@hotmail.com>
2026-05-05 12:41:47 +0200

    Fixes: Button Component

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import styles from './Button.module.css';
 
+ type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+   children: React.ReactNode;
+ };
+
 export default function Button({
   children,
-  onClick,
-}: {
-  children: React.ReactNode;
-  onClick: () => void;
-}) {
+  className,
+  type = 'button',
+  ...props
+}: ButtonProps) {
   return (
     <button
-      className={styles.button}
-      onClick={onClick}
+      className={className ? `${styles.button} ${className}` : styles.button}
+      type={type}
+      {...props}
     >
       {children}
     </button>
