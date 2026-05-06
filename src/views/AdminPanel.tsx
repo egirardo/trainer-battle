6141e85a15adc1c596dab5c2b1ae10f8e170d06e
Your Name <laura.kotlinska.96@gmail.com>
2026-05-06 10:56:13 +0200

    Add Item and Move rows and AdminLogin components for move and item management

--- src/views/AdminPanel.tsx
+++ src/views/AdminPanel.tsx
@@ -1,4 +1,6 @@
 import CreatureFormRow from "../components/molecules/CreatureFormRow";
+import MoveFormRow from "../components/molecules/MoveFormRow";
+import ItemFormRow from "../components/molecules/ItemFormRow";
 
 export default function AdminPanel(){
     return(
@@ -24,6 +26,50 @@ export default function AdminPanel(){
                         </thead>
                         <tbody>
                             <CreatureFormRow />
+                        </tbody>
+                    </table>
+                </form>
+            </section>
+
+            <section>
+                <h2>Moves:</h2>
+                <form>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Type</th>
+                                <th>Power</th>
+                                <th>Accuracy</th>
+                                <th>Effect</th>
+                                <th>Description</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <MoveFormRow />
+                        </tbody>
+                    </table>
+                </form>
+            </section>
+
+            <section>
+                <h2>Items:</h2>
+                <form>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th>Effect</th>
+                                <th>Price</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <ItemFormRow />
                         </tbody>
                     </table>
                 </form>
