a6eeb702abd2655b3e4490afb7b46d9396d1040b
Your Name <laura.kotlinska.96@gmail.com>
2026-05-08 09:37:13 +0200

    Add StartScreen component with styling and navigation links

--- src/views/StartScreen.tsx
+++ src/views/StartScreen.tsx
@@ -1,16 +1,35 @@
 import { Link } from "react-router-dom";
+import styles from './StartScreen.module.css'
+import testcreature1 from '@/assets/sprites/creatures/test-character.png';
+import testcreature2 from '@/assets/sprites/creatures/test-character2.png';
 
 export default function StartScreen(){
     return(
         <main>
             <h1>
-                Trainer Battle
+                <span className={styles.gameLogo}>- Trainer -</span>
+                <span className={styles.gameLogo}>Battle</span>
             </h1>
-            <h2>
-                Main screen
-            </h2>
-            <p>Login or start new game</p>
-            <Link to={"/character-select"}>Start - Char select</Link>
+            <div className={styles.logoImgContainer}>
+                <img className={styles.logoImg} src={testcreature1} />
+                <img className={styles.logoImg} src={testcreature2} />
+            </div>
+            <div className={styles.navContainer}>
+                <Link 
+                    className={styles.startButton} 
+                    to={"/character-select"}>
+                        New game
+                </Link>
+                <Link 
+                    className={styles.startButton} 
+                    to={"/login"}>
+                        Continue
+                </Link>
+            </div>
+            <div className={styles.extrasContainer}>
+                <Link to="#">Credits</Link>
+                <Link to="#">How do I play?</Link>
+            </div>
         </main>
     )
 }
