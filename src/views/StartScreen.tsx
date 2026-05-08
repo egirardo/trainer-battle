bf7b6539520d50d3c997126355c6ec1e677334f6
Your Name <laura.kotlinska.96@gmail.com>
2026-05-08 12:32:55 +0200

    Refactor StartScreen layout and update button component usage

--- src/views/StartScreen.tsx
+++ src/views/StartScreen.tsx
@@ -1,34 +1,44 @@
 import { Link } from "react-router-dom";
 import styles from './StartScreen.module.css'
-import testcreature1 from '@/assets/sprites/creatures/test-character.png';
-import testcreature2 from '@/assets/sprites/creatures/test-character2.png';
+import testcreature1 from '@/assets/sprites/creatures/fire-creature.png';
+import testcreature2 from '@/assets/sprites/creatures/water-creature.png';
+import Button from "@/components/atoms/button";
 
 export default function StartScreen(){
     return(
-        <main>
-            <h1>
-                <span className={styles.gameLogo}>- Trainer -</span>
-                <span className={styles.gameLogo}>Battle</span>
-            </h1>
-            <div className={styles.logoImgContainer}>
-                <img className={styles.logoImg} src={testcreature1} />
-                <img className={styles.logoImg} src={testcreature2} />
+        <main className={styles.mainScreen}>
+            <div>
+                <h1 className={styles.logoContainer}>
+                    <span className={styles.gameLogo}>- Trainer -</span>
+                    <span className={styles.gameLogo}>Battle</span>
+                </h1>
+                <div className={styles.logoImgContainer}>
+                    <img className={styles.logoImg} src={testcreature1} />
+                    <img className={styles.logoImg} src={testcreature2} />
+                </div>
             </div>
             <div className={styles.navContainer}>
-                <Link 
-                    className={styles.startButton} 
-                    to={"/character-select"}>
-                        New game
+                <Link to={"/character-select"}>
+                       <Button className={styles.startButton}>
+                            New game
+                        </Button>
                 </Link>
-                <Link 
-                    className={styles.startButton} 
-                    to={"/login"}>
-                        Continue
+                <Link to={"/login"}>
+                       <Button className={styles.startButton}>
+                            Continue
+                        </Button>
                 </Link>
             </div>
             <div className={styles.extrasContainer}>
-                <Link to="#">Credits</Link>
-                <Link to="#">How do I play?</Link>
+                {
+                    // TODO: Pop up creds and instructions
+                }
+                <Button className={`${styles.startButton} ${styles.small}`}>
+                    Credits
+                </Button>                       
+                <Button className={`${styles.startButton} ${styles.small}`}>
+                    How do I play?
+                </Button>
             </div>
         </main>
     )
