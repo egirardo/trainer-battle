deead99f2ee7cd38e2052e286e6e288ad1142bb6
Your Name <laura.kotlinska.96@gmail.com>
2026-05-10 15:37:51 +0200

    Refactor navigation to use ROUTES constant in LobbyScreen

--- src/views/LobbyScreen.tsx
+++ src/views/LobbyScreen.tsx
@@ -1,3 +1,4 @@
+import { ROUTES } from "@/routes";
 import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLobby } from "@/hooks/useLobby";
