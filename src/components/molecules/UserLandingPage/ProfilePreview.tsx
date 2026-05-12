e02f6837cf0732648b3467aed5a52c744846d6d4
Elsa Girardo <egirardo@hotmail.com>
2026-05-13 00:00:29 +0200

    Feature: Updated Trainer Avatars

--- src/components/molecules/UserLandingPage/ProfilePreview.tsx
+++ src/components/molecules/UserLandingPage/ProfilePreview.tsx
@@ -1,8 +1,8 @@
 import styles from './ProfilePreview.module.css';
 import type { Trainer, TrainerGender } from '@/models/models';
-import femaleTrainer from '@/assets/sprites/trainers/test-avatar-f.svg';
-import maleTrainer from '@/assets/sprites/trainers/test-avatar-m.svg';
-import nbTrainer from '@/assets/sprites/trainers/test-avatar-nb.png';
+import femaleTrainer from '@/assets/sprites/trainers/avatar-f.svg';
+import maleTrainer from '@/assets/sprites/trainers/avatar-m.svg';
+import nbTrainer from '@/assets/sprites/trainers/avatar-nb.png';
 import { Link } from 'react-router-dom';
 
 const trainerImages: Record<TrainerGender, string> = {
