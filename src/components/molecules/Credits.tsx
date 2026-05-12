d34088d645fc9bc484bea293991b68ff59f41087
Your Name <laura.kotlinska.96@gmail.com>
2026-05-12 14:49:10 +0200

    Initial content

---
+++ src/components/molecules/Credits.tsx
@@ -0,0 +1,21 @@
+import CloseButton from "../atoms/headerButtons/CloseButton";
+import StickyHeader from "../atoms/StickyHeader";
+
+interface Props {
+    onClose?: () => void;
+}
+
+export default function Credits({ onClose }: Props){
+    return(
+        <main>
+            <StickyHeader 
+                label="Credits"
+                action={<CloseButton onClick={onClose}/>}
+            />
+            <p>Hardwork of:</p>
+            <p>John Ahlenhed</p>
+            <p>Laura Kotlinska</p>
+            <p>Elsa Girardo</p>
+        </main>
+    )
+}
