cb48d0a4ea96fbf4cecb5e2efef18722f2e99016
Your Name <laura.kotlinska.96@gmail.com>
2026-05-12 14:50:01 +0200

    Delete paragraph about switch function

--- src/components/molecules/gameInstructions/BattleInstructions.tsx
+++ src/components/molecules/gameInstructions/BattleInstructions.tsx
@@ -5,7 +5,6 @@ export default function BattleInstructions(){
             <p>Battles are turn-based. On your turn, choose one of four actions:</p>
             <p><span className={styles.strong}>- Fight</span> to attack with one of your moves</p>
             <p><span className={styles.strong}>- Bag</span> to use an item on your creature</p>
-            <p><span className={styles.strong}>- Switch</span> to swap to another creature</p>
             <p><span className={styles.strong}>- Run</span> to escape the battle</p>
             <div className={styles.toolTip}>
                 <p>You can run from any battle, but you won't earn stamps. Save running for fights you can't win!</p>
